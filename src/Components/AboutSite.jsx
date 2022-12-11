b62b50e71cc21f0452b38582307d7919672da9d8
farshadBish <farshadota@gmail.com>
2022-12-11 12:49:35 +0100

    Update AboutSite.jsx

--- src/Components/AboutSite.jsx
+++ src/Components/AboutSite.jsx
@@ -12,7 +12,7 @@ const AboutSite = () => {
                 <h5>Here I inform about the techs that I used and what you can achive in this site.</h5></span>
               </div>
                 </Col>
-                <Col md={12} lg={6}  className="pl-0 mb-2 pr-0">
+                <Col md={12} lg={6}  className="pl-0 mb-lg-2 pr-0">
                 <div className="firstPic"></div>
                 </Col>
                 {/* second col */}
@@ -26,7 +26,7 @@ const AboutSite = () => {
                     <p className="aboutText">For this site i used <b>1.</b> Impala api that provide me to GET Hotels and also some details about them , <b>2.</b> my own back-end made with <b>Express and mongoDB</b> as database that has the possibility to register/login as admin and user using JWT token and also possibility to book hotels ,  manage the booked hotels and also change the the own user credentials, for admin he can modify the users + modifying all the booking made.<b>3. </b>I used AWS EC2 for run my projects using Nginx and also Heroku for running my back-end <b>4. </b>For front-end I used technologies such as <b>React, Bootstrap</b> and also some react packages such as littebit of React spring and also <b>0 Templates</b> has been used and as i know the site is fully responsive. <b>Workflow:</b> Making an account first because without that, aint possible to book bro after you can search for hotels by the name of country and/or by name of the hotel, then you see some details and information about the hotel if see yourself interested you can click on Book Now button to start booking, afterwards you go to another window where the booking process begins (you dont need to put something in payment page) after your booking is done from navbar you can go to ur profile and see your booking <b>also the Weather is still under development.</b> If interested to see how admin works try to log in as : <b>Email : farshadota@gmail.com<br/> Password: 1234</b></p>
                     </div>
                 </Col>
-                <Col xl={3} lg={2} className="pl-0 pr-0 mb-2">
+                <Col xl={3} lg={2} className="pl-0 pr-0 mb-lg-2">
                     <div className="middleRight"></div>
                 </Col>
                 {/* third col */}
