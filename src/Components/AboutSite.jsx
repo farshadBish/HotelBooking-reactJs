a834208616e142a3c6307405b24c9da5b09cedaf
farshadBish <farshadota@gmail.com>
2023-01-14 16:19:42 +0100

    Update AboutSite.jsx

--- src/Components/AboutSite.jsx
+++ src/Components/AboutSite.jsx
@@ -1,10 +1,8 @@
 import { Col, Container, Row } from "react-bootstrap";
 import '../styles/aboutSite.css'
-import {animated , useSpring,useScroll} from "@react-spring/web";
+import {animated , useSpring} from "@react-spring/web";
 import { FaChevronDown } from "react-icons/fa";
 const AboutSite = () => {
-
-    const { scrollYProgress } = useScroll()
 
     const firstSpring = useSpring({
         from : { opacity:0 , x:-200,},
@@ -84,7 +82,7 @@ const AboutSite = () => {
                     <div className="middleRight d-flex align-items-center justify-content-center text-white"><div><FaChevronDown className="flashFont d-block"/><FaChevronDown className="flashFont d-block"/><FaChevronDown className="flashFont d-block"/></div></div>
                 </animated.div>
                 {/* third col */}
-                <animated.div classname="col-lg-6 col-md-12 pr-0 pl-0" style={{ opacity: scrollYProgress }}>
+                <animated.div className="col-lg-6 col-md-12 pr-0 pl-0">
                 <div className="secondPic"></div>
                 </animated.div>
                 <Col md={12} lg={6} className="pl-0 pr-0 mb-5">
