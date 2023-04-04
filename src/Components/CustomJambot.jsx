4e449f5dd65a4839131bedaf1bf13dec6b821fc3
farshadBish <farshadota@gmail.com>
2023-04-04 22:16:48 +0200

    Update CustomJambot.jsx

--- src/Components/CustomJambot.jsx
+++ src/Components/CustomJambot.jsx
@@ -7,8 +7,72 @@ import {
 } from "react-bootstrap";
 import "../styles/customNavbar.css";
 import "../styles/customJamb.css";
+import {animated , useSpring} from "@react-spring/web";
+import { useState } from "react";
 
 function CustomJambot() {
+
+  const firstSpring = useSpring({
+    from : { scale: 0 , rotate: 0},
+    to: [{scale: 1.2,rotate: 180},
+    {scale: 1,rotate: 360}],
+    config: {
+        duration: 800
+    },
+});
+const secondSpring = useSpring({
+  from : { scale: 0 , rotate: 0},
+  to: [{scale: 1.2,rotate: 180},
+  {scale: 1,rotate: 360}],
+  config: {
+      duration: 800
+  },
+  delay: 200
+});
+const thirdSpring = useSpring({
+  from : { scale: 0 , rotate: 0},
+  to: [{scale: 1.2,rotate: 180},
+  {scale: 1,rotate: 360}],
+  config: {
+      duration: 800
+  },
+  delay: 300
+});
+const forthSpring = useSpring({
+  from : { scale: 0 , rotate: 0},
+  to: [{scale: 1.2,rotate: 180},
+  {scale: 1,rotate: 360}],
+  config: {
+      duration: 800
+  },
+  delay: 400
+});
+const fifthSpring = useSpring({
+  from : { scale: 0 , rotate: 0},
+  to: [{scale: 1.2,rotate: 180},
+  {scale: 1,rotate: 360}],
+  config: {
+      duration: 800
+  },
+  delay: 500
+});
+// pic change
+const [firstPic,setFirstPic] = useState("https://media.discordapp.net/attachments/713873647550464000/991280660314591303/victorops-logo-clr.png")
+const [secondPic,setSecondPic] = useState("small-1.jpg")
+const [thirdPic,setThirdPic] = useState("Small-2.jpg")
+const [forthPic,setForthPic] = useState("Small-3.jpg")
+const [fifthPic,setFifthPic] = useState("https://cdn.discordapp.com/attachments/685210402577055758/1046810021469630464/Small-4.jpg")
+const [sixthPic,setSixthPic] = useState("https://cdn.discordapp.com/attachments/685210402577055758/1046810049223327854/Small-5.jpg")
+
+const [changedPic,setChangedPic] = useState(null)
+const [active,setActive] = useState(false)
+const picChanger = (picUrl) => {
+  picUrl="dick"
+  console.log(picUrl);
+  setActive(true);
+  setChangedPic(picUrl);
+  setFirstPic(changedPic);
+}
   return (
     <Jumbotron
       fluid
@@ -20,7 +84,7 @@ function CustomJambot() {
           <Col md={12} className="">
             <div className="gradient" style={{ borderRadius: "20px" }}>
               <img
-                src="https://media.discordapp.net/attachments/713873647550464000/991280660314591303/victorops-logo-clr.png"
+                src={firstPic}
                 alt=""
                 className="logo"
               />
@@ -47,11 +111,11 @@ function CustomJambot() {
             Here we provide you the available hotels and the weather temperature
             of your desired city.
           </p>
-          <img src="small-1.jpg" alt="" className="radius ml-4" />
-          <img src="Small-2.jpg" alt="" className="radius" />
-          <img src="Small-3.jpg" alt="" className="radius smallPicDisapear" />
-          <img src="https://cdn.discordapp.com/attachments/685210402577055758/1046810021469630464/Small-4.jpg" alt="" className="radius smallPicDisapear" />
-          <img src="https://cdn.discordapp.com/attachments/685210402577055758/1046810049223327854/Small-5.jpg" alt="" className="radius smallPicDisapear" />
+          <animated.img onClick={()=>console.log("dick")} style={firstSpring} src={secondPic} alt="" className="radius ml-4" />
+          <animated.img  style={secondSpring} src={thirdPic} alt="" className="radius" />
+          <animated.img  style={thirdSpring} src={forthPic} alt="" className="radius smallPicDisapear" />
+          <animated.img  style={forthSpring} src={fifthPic}  alt="" className="radius smallPicDisapear" />
+          <animated.img  style={fifthSpring} src={sixthPic} alt="" className="radius smallPicDisapear" />
         </div>
       </Container>
     </Jumbotron>
